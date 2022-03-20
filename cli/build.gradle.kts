d74a9b795c0573f931050a13f9d2f6a0a8b85b88
Saket <saket@squareup.com>
2022-03-20 18:28:59 -0400

    Prepare next development version

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -8,11 +8,12 @@ application {
 }
 
 dependencies {
-  implementation(project(":unfurl"))
-  implementation(project(":unfurl-social"))
+  implementation("me.saket.unfurl:unfurl:1.6.0")
+  implementation("me.saket.unfurl:unfurl-social:1.6.0")
   implementation("com.github.ajalt.clikt:clikt:3.4.0")
 
   // The version of OkHttp used by :unfurl fails to download AndroidPolice.com
   // articles with "StreamResetException: stream was reset: PROTOCOL_ERROR" errors.
+  // Updating to OkHttp v5.x seems to fix them.
   implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.6")
 }
--- unfurl/build.gradle.kts
+++ unfurl/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 apply(plugin = "com.vanniktech.maven.publish")
 
 dependencies {
-  api("com.squareup.okhttp3:okhttp:4.9.0")
+  api("com.squareup.okhttp3:okhttp:4.9.0")  // Updating to 5.x? See cli/build.gradle.kts.
   implementation("org.jsoup:jsoup:1.14.3")
 }
