2aa2be9613e78837900c708ac60ed122185e6007
Saket <saket@squareup.com>
2022-08-10 23:58:26 -0400

    Kotlin 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
   }
 
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     classpath("com.vanniktech:gradle-maven-publish-plugin:0.19.0")
     classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
   }
