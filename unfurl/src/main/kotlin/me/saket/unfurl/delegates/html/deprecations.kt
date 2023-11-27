aadb593c46448fe51d0fd09aa9b4a31d213783e7
laatonwalabhoot <aishwarya.mailin@gmail.com>
2023-11-27 05:48:14 +0530

    chore: remove extra module

--- unfurl/src/main/kotlin/me/saket/unfurl/delegates/html/deprecations.kt
+++
@@ -1,7 +0,0 @@
-package me.saket.unfurl.delegates.html
-
-@Deprecated(
-  message = "Renamed to HtmlTagsBasedUnfurler",
-  replaceWith = ReplaceWith("me.saket.unfurl.extension.HtmlTagsBasedUnfurler")
-)
-typealias HtmlTagsBasedUnfurler = me.saket.unfurl.extension.HtmlTagsBasedUnfurler
