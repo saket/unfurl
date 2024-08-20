84d864e3750270263bdcf4918e61cd0caa1648a5
Saket Narayan <saket@saket.me>
2024-08-20 16:58:51 -0400

    Prepare to release v2.2.0

--- unfurl/build.gradle.kts
+++ unfurl/build.gradle.kts
@@ -34,7 +34,7 @@ mavenPublishing {
   signAllPublications()
   publishToMavenCentral(automaticRelease = true)
 
-  coordinates("me.saket.unfurl", "unfurl", "2.2.0-SNAPSHOT")
+  coordinates("me.saket.unfurl", "unfurl", "2.2.0")
   pom {
     name = "unfurl"
     description = "Generate preview of links, inspired by Slack"
